984d0a04fac8da229dc53267c58a2dc4b4b4450e
Aasha Shakya <178453028+Ash-1412@users.noreply.github.com>
2024-11-05 20:27:26 +0000

    initial commit

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,35 @@
 // import statements
+import java.util.*;
+import java.io.*;
 
-public class PriorityQueueLab {
+public class PriorityQueueLab
+{
+    public static void main(String[] args)
+    {
+        File file = new File("RandIntegers.txt");
+        Scanner scan;
+        PriorityQueue<Integer> smolToLarge = new PriorityQueue<Integer>();
+
+        try
+        {
+            scan = new Scanner(file);
+
+            while(scan.hasNextInt())
+            {
+                smolToLarge.add(scan.nextInt());
+            }           
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error");
+        }
+
+        System.out.println("Contents: " + smolToLarge);
+    }
   
 }
 
